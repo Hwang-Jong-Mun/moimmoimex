d4425c7b5385883672073aa9cb481e970a4d8d57
Hwang-Jong-Mun <hjm8539636@naver.com>
2023-05-02 09:55:26 +0900

    Initial commit

---
+++ src/main/java/javatreeex/moimmoimex/MoimmoimexApplication.java
@@ -0,0 +1,13 @@
+package javatreeex.moimmoimex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimmoimexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoimmoimexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javatreeex/moimmoimex/MoimmoimexApplicationTests.java
@@ -0,0 +1,13 @@
+package javatreeex.moimmoimex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimmoimexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
